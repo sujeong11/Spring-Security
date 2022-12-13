6b046e8fe4115b5fd6990e9e44ebf8ccaade56fa
sujeong11 <qalzm7351@gmail.com>
2022-12-13 22:27:58 +0900

    Initial commit

---
+++ src/test/java/com/sujeong/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujeong.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
